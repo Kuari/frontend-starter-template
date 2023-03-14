08ce3310a9c8813dec8fabea6bef0317052ff3f0
kuari <kuari@justmylife.cc>
2023-03-14 23:56:28 +0800

    feat:svg load & formater

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,12 @@
+<script lang="ts" setup>
+import IconPower from '@/assets/svg/power.svg?component'
+import IconSmile from '@/assets/svg/smile.svg?component'
+</script>
+
 <template>
-  <div>
-    home
+  <div class="p-2 flex items-center">
+    <IconSmile class="fill-yellow-200 stroke-red-500" style="fill: #343334;color: blue;" />
+    Hello!
+    <IconPower />
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import WindiCSS from 'vite-plugin-windicss'
+import svgLoader from 'vite-svg-loader'
 import { version as pkgVersion } from './package.json'
 
 process.env.VITE_APP_VERSION = pkgVersion
@@ -15,6 +16,7 @@ export default defineConfig({
   plugins: [
     vue(),
     WindiCSS(),
+    svgLoader(),
     AutoImport({
       imports: [
         'vue',
